11fc56706fd2c5485e24018a90f3ad342dea63ad
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2024-09-30 15:11:16 +0200

    Update usePokestopMarker.js

--- src/features/pokestop/usePokestopMarker.js
+++ src/features/pokestop/usePokestopMarker.js
@@ -356,8 +356,8 @@ export function usePokestopMarker({
                     bottom: ${
                       baseSize * 0.6 * eventMod.offsetY + showcaseSizes[i] * i
                     }px;
-                    left: ${eventMod.offsetX * 5}%;
-                    transform: translateX(-50%);
+                    left: ${eventMod.offsetX * 10}%;
+                    transform: translateX(-70%);
                   "
                 />
                 ${
