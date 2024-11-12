220ef0c42236a1d073d3ce5d747a071159db505f
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2024-11-12 13:13:26 +0100

    Update StationPopup.jsx

--- src/features/station/StationPopup.jsx
+++ src/features/station/StationPopup.jsx
@@ -60,22 +60,20 @@ export function StationPopup(station) {
       </Box>
       {!!station.battle_level && <StationRating {...station} />}
       <StationMedia {...station} />
-      {!!station.is_battle_available &&
-        station.battle_start < Date.now() / 1000 &&
-        station.battle_end > Date.now() / 1000 && (
-          <ExpandCollapse>
-            <StationAttackBonus {...station} />
-            <ExpandWithState
-              field="popups.stationExtras"
-              disabled={!station.total_stationed_pokemon}
-            />
-            <CollapseWithState
-              field="popups.stationExtras"
-              in={!!station.total_stationed_pokemon}
-            >
-              <StationMons {...station} />
-            </CollapseWithState>
-          </ExpandCollapse>
+      {!!station.is_battle_available && station.battle_start < Date.now() / 1000 && (
+        <ExpandCollapse>
+          <StationAttackBonus {...station} />
+          <ExpandWithState
+            field="popups.stationExtras"
+            disabled={!station.total_stationed_pokemon}
+          />
+          <CollapseWithState
+            field="popups.stationExtras"
+            in={!!station.total_stationed_pokemon}
+          >
+            <StationMons {...station} />
+          </CollapseWithState>
+        </ExpandCollapse>
       )}
       <StationContent {...station} />
       <Footer lat={station.lat} lon={station.lon} />
