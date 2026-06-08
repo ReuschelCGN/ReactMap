88683537e523b2ce5fadf8bec912afa17c7fe637
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2026-06-09 00:55:02 +0200

    Update Quests.jsx

--- src/features/drawer/pokestops/Quests.jsx
+++ src/features/drawer/pokestops/Quests.jsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import ListItem from '@mui/material/ListItem'
 
 import { useStorage } from '@store/useStorage'
+import { useMemory } from '@store/useMemory'
 import { QUEST_SETS } from '@assets/constants'
 import { MultiSelectorStore } from '@components/inputs/MultiSelector'
 
@@ -11,7 +12,8 @@ import { MultiSelectorList, SelectorListMemo } from '../components/SelectorList'
 
 const BaseQuestQuickSelect = () => {
   const enabled = useStorage((s) => !!s.filters?.pokestops?.quests)
-  return (
+  const { enableQuestSetSelector } = useMemory.getState().config.misc
+  return enableQuestSetSelector ? (
     <CollapsibleItem open={enabled}>
       <ListItem>
         <MultiSelectorStore
@@ -19,6 +21,25 @@ const BaseQuestQuickSelect = () => {
           items={QUEST_SETS}
         />
       </ListItem>
+      <MultiSelectorList tabKey="quests">
+        <SelectorListMemo
+          key="items"
+          category="pokestops"
+          subCategory="quests"
+          label="search_quests"
+          height={350}
+        />
+        <SelectorListMemo
+          key="pokemon"
+          category="pokestops"
+          subCategory="pokemon"
+          label="search_quests"
+          height={350}
+        />
+      </MultiSelectorList>
+    </CollapsibleItem>
+  ) : (
+    <CollapsibleItem open={enabled}>
       <MultiSelectorList tabKey="quests">
         <SelectorListMemo
           key="items"
